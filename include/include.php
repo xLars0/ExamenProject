ea69ef5d5743e6eaa47cf84694f8ffc2439d0447
Lars Methorst <larshorst@horst@hotmail.com>
2018-12-19 12:30:53 +0100

    First Commit

---
+++ include/include.php
@@ -0,0 +1,9 @@
+<?php
+$includePath = "../";
+include $includePath.('db/dbConnect.php');
+include $includePath.('php/weergaves/header.php');
+?>
+
+<link rel="icon" type="image/jpg" href="../img/emblem.png"/>
+<link href='../css/w3.css' type='text/css' rel='stylesheet'/>
+<link href='../css/css.css' type='text/css' rel='stylesheet'/>
