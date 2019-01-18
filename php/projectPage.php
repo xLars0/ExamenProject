9fcc0895c10a65f4d485f0809353fed1aa3dcffd
Lars Methorst <larshorst@horst@hotmail.com>
2019-01-18 12:58:18 +0100

    projectPage

---
+++ php/projectPage.php
@@ -0,0 +1,20 @@
+<?php
+$includePath = "../";
+include $includePath.('include/include.php');
+$searchUrl = "functions/projectSearch.php";
+?>
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+
+<script> var searchUrl = "functions/projectSearch.php"; </script>
+<?php include ('functions/projectFunction.php'); ?>
+
+<html>
+    <head>
+        <title>Company - Schadelijst</title>
+    </head>
+
+    <body>
+
+    </body>
+</html>
